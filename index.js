21ca7ea9c7942f5d54d273fc58f6dd99113a5516
Anna Agte <matiasrustagte@gmail.com>
2023-09-22 03:45:11 +0500

    feat: almost all

---
+++ index.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs';
+
+import Application from './lib/Application.js';
+
+export default function (currentPath, components) {
+  let appDir = '';
+
+  const currentDir = path.dirname(fileURLToPath(currentPath));
+  if (fs.existsSync(path.resolve(currentDir, 'api'))) {
+    appDir = currentDir;
+  } else if (currentDir.has('/api/')) {
+    appDir = currentDir.slice(0, currentDir.lastIndexOf('/api/'));
+  } else {
+    throw Error('Не получается определить папку приложения');
+  }
+
+  return new Application({ path: appDir, components });
+};
